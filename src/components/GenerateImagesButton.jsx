b817db6cdd65e3ae857292a8c375b2ab88390ae6
Edison Crisostomo <emc.ai.studio@gmail.com>
2025-07-26 17:38:35 +1000

    header and footer fixed

--- src/components/GenerateImagesButton.jsx
+++ src/components/GenerateImagesButton.jsx
@@ -94,7 +94,7 @@ function GenerateImagesButton({ messages }) {
     <>
       <button
         onClick={generateScreenshots}
-        className="w-full px-4 py-2 rounded bg-indigo-500 text-white hover:bg-indigo-600 transition"
+        className="w-full px-4 py-2 rounded bg-gray-600 text-white hover:bg-gray-700 transition"
         disabled={generating}
       >
         {generating ? 'Generating...' : 'Generate Images'}
@@ -122,16 +122,99 @@ function GenerateImagesButton({ messages }) {
             <div style={{
               height: `${HEADER_HEIGHT}px`,
               display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
               alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: '0 12px',
               borderBottom: '1px solid #eee',
-              fontSize: '16px',
-              fontWeight: 'bold'
+              fontFamily: 'system-ui',
+              position: 'relative',
+              backgroundColor: '#fafafa'
             }}>
-              Jessica
-              <div style={{ fontSize: '12px', color: '#888' }}>9:41 AM</div>
+
+              {/* Time (top left) */}
+              <div style={{
+                position: 'absolute',
+                top: '4px',
+                left: '12px',
+                fontSize: '11px',
+                color: '#333',
+                fontWeight: 'bold'
+              }}>
+                22:11
+              </div>
+
+              {/* Top right indicators */}
+              <div style={{
+                position: 'absolute',
+                top: '4px',
+                right: '12px',
+                display: 'flex',
+                alignItems: 'center',
+                gap: '4px',
+                color: '#333'
+              }}>
+                {/* Phone Signal */}
+            <svg width="11" height="11" viewBox="0 0 1200 1200" fill="currentColor">
+
+                  <path d="M960,1200V0h240v1200H960z M640,300h240v900H640V300z M320,600h240v600H320V600L320,600z M0,900h240v300H0V900z"/>
+                </svg>
+
+                {/* Wi-Fi */}
+                <svg width="16" height="16" viewBox="0 0 42 42" fill="currentColor">
+                  <path d="M25,31.041c-2.21-2.191-5.79-2.191-8,0L21,35L25,31.041z M33,23.12c-6.63-6.562-17.37-6.562-24,0
+                  l4,3.959c4.42-4.37,11.58-4.37,16,0L33,23.12z M41,15.199c-11.05-10.932-28.95-10.932-40,0l4,3.959c8.84-8.751,23.16-8.751,32,0
+                  L41,15.199z"/>
+                </svg>
+
+                {/* Battery */}
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
+                  <path d="M2 6h10v4H2V6z"/>
+                  <path d="M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h10zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z"/>
+                </svg>
+              </div>
+
+              {/* Back arrow */}
+              <div style={{
+                color: '#007AFF',
+                fontSize: '24px',
+                paddingRight: '10px'
+              }}>
+                ‹
+              </div>
+
+              {/* Avatar + Name */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                flexGrow: 1
+              }}>
+                <div style={{
+                  width: '36px',
+                  height: '36px',
+                  borderRadius: '50%',
+                  backgroundColor: '#bcd0f5',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  fontSize: '20px',
+                  color: '#4666a8',
+                  marginBottom: '2px'
+                }}>
+                  👤
+                </div>
+                <div style={{ fontSize: '13px', fontWeight: '500' }}>Brooke &lt;3</div>
+              </div>
+
+              {/* Video call icon */}
+              <div style={{ width: '24px', height: '24px', marginLeft: 'auto' }}>
+                <svg viewBox="0 0 24 24" fill="#007AFF" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23.458,5.11a1,1,0,0,0-1.039.077L17,9.057V6a2,2,0,0,0-2-2H2A2,2,0,0,0,0,6V18a2,2,0,0,0,2,2H15a2,2,0,0,0,2-2V14.943l5.419,3.87A.988.988,0,0,0,23,19a1.019,1.019,0,0,0,.458-.11A1,1,0,0,0,24,18V6A1,1,0,0,0,23.458,5.11ZM2,18V6H15V18Zm20-1.943-5-3.572v-.97l5-3.572Z"/>
+                </svg>
+              </div>
             </div>
+
+
 
             {/* Message Body */}
             <div style={{
@@ -146,27 +229,45 @@ function GenerateImagesButton({ messages }) {
               <ChatPreview messages={chunks[pageIndex]} />
             </div>
 
-            {/* Footer */}
-            <div style={{
-              height: `${FOOTER_HEIGHT}px`,
-              borderTop: '1px solid #eee',
-              padding: '10px 12px',
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px'
-            }}>
-              <div style={{
-                flex: 1,
-                backgroundColor: '#f2f2f2',
-                borderRadius: '20px',
-                padding: '6px 12px',
-                fontSize: '14px',
-                color: '#999'
-              }}>
-                iMessage
-              </div>
-              <div style={{ color: '#007AFF', fontWeight: 'bold' }}>Send</div>
-            </div>
+{/* Footer */}
+<div style={{
+  height: `${FOOTER_HEIGHT}px`,
+  borderTop: '1px solid #eee',
+  padding: '10px 12px',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px'
+}}>
+  {/* Camera Icon */}
+  <div style={{ width: 24, height: 24 }}>
+    <svg width="24" height="24" viewBox="0 -2 32 32" xmlns="http://www.w3.org/2000/svg">
+      <path fill="#aaa" d="M286,471 L283,471 L282,469 C281.411,467.837 281.104,467 280,467 L268,467 C266.896,467 266.53,467.954 266,469 L265,471 L262,471 C259.791,471 258,472.791 258,475 L258,491 C258,493.209 259.791,495 262,495 L286,495 C288.209,495 290,493.209 290,491 L290,475 C290,472.791 288.209,471 286,471 Z M274,491 C269.582,491 266,487.418 266,483 C266,478.582 269.582,475 274,475 C278.418,475 282,478.582 282,483 C282,487.418 278.418,491 274,491 Z M274,477 C270.687,477 268,479.687 268,483 C268,486.313 270.687,489 274,489 C277.313,489 280,486.313 280,483 C280,479.687 277.313,477 274,477 L274,477 Z" transform="translate(-258 -467)" />
+    </svg>
+  </div>
+
+  {/* Input bubble */}
+  <div style={{
+    flex: 1,
+    height: 28,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 16,
+    padding: '0 12px',
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: 12,
+    color: '#999'
+  }}>
+    iMessage
+  </div>
+
+  {/* Voice Icon */}
+  <div style={{ width: 24, height: 24 }}>
+    <svg fill="#aaa" viewBox="0 0 56 56" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M 24.5898 49.5742 C 25.5508 49.5742 26.2539 48.8477 26.2539 47.9336 L 26.2539 8.0664 C 26.2539 7.1524 25.5508 6.4258 24.5898 6.4258 C 23.6524 6.4258 22.9492 7.1524 22.9492 8.0664 L 22.9492 47.9336 C 22.9492 48.8477 23.6524 49.5742 24.5898 49.5742 Z M 38.2305 44.8867 C 39.1680 44.8867 39.8711 44.1367 39.8711 43.2227 L 39.8711 12.7773 C 39.8711 11.8633 39.1680 11.1133 38.2305 11.1133 C 37.2930 11.1133 36.5664 11.8633 36.5664 12.7773 L 36.5664 43.2227 C 36.5664 44.1367 37.2930 44.8867 38.2305 44.8867 Z M 17.7930 41.0898 C 18.7305 41.0898 19.4336 40.3633 19.4336 39.4492 L 19.4336 16.5508 C 19.4336 15.6367 18.7305 14.9102 17.7930 14.9102 C 16.8320 14.9102 16.1289 15.6367 16.1289 16.5508 L 16.1289 39.4492 C 16.1289 40.3633 16.8320 41.0898 17.7930 41.0898 Z M 31.4102 38.5586 C 32.3476 38.5586 33.0742 37.8320 33.0742 36.9180 L 33.0742 19.0820 C 33.0742 18.1680 32.3476 17.4414 31.4102 17.4414 C 30.4727 17.4414 29.7695 18.1680 29.7695 19.0820 L 29.7695 36.9180 C 29.7695 37.8320 30.4727 38.5586 31.4102 38.5586 Z M 45.0508 34.3633 C 45.9883 34.3633 46.6914 33.6133 46.6914 32.6992 L 46.6914 23.3008 C 46.6914 22.3867 45.9883 21.6367 45.0508 21.6367 C 44.0898 21.6367 43.3867 22.3867 43.3867 23.3008 L 43.3867 32.6992 C 43.3867 33.6133 44.0898 34.3633 45.0508 34.3633 Z M 10.9727 32.5117 C 11.9102 32.5117 12.6133 31.7851 12.6133 30.8711 L 12.6133 25.1289 C 12.6133 24.2149 11.9102 23.4883 10.9727 23.4883 C 10.0117 23.4883 9.3086 24.2149 9.3086 25.1289 L 9.3086 30.8711 C 9.3086 31.7851 10.0117 32.5117 10.9727 32.5117 Z"/>
+    </svg>
+  </div>
+</div>
+
           </div>
         )}
       </div>
